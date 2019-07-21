5d48653ce8dadb498a34a76b720c4bafeee1101b
Slay3r <philip20001@outlook.de>
2019-07-21 17:06:18 +0200

    added new features
    - chuck norris jokes
    - geolocate ip
    - programming jokes
    - international space station position

--- run.php
+++ run.php
@@ -71,6 +71,105 @@ $booru->registerSubCommand(
   ]
 );
 
+function isJson($string) {
+  return is_object(json_decode($string));
+}
+
+// chuck norris joke
+$discord->registerCommand(
+  'chuck',
+  function ($message){
+    $api_ret = file_get_contents("https://api.chucknorris.io/jokes/random");
+    if ( !isJson($api_ret) )
+      return "api error";
+    else
+      return json_decode($api_ret,true)["value"];
+  },
+  [
+  'description' => 'random Chuck-Norris-Joke'
+  ]
+);
+
+// geo ip
+$discord->registerCommand(
+  'geoip',
+  function ($message, $params){
+    if (count($params))
+    {
+      $api_ret = file_get_contents("http://ip-api.com/json/".$params[0]);
+      if ( !isJson($api_ret) )
+        return "api error";
+      else
+      {
+        $out  = "```";
+        $data = json_decode($api_ret);
+        foreach ($data as $key => $value)
+        {
+          $out .= $key . " => " . $value . "\n";
+        }
+        $out .= "```";
+        return $out;
+      }
+    }
+    else
+    {
+      return "no ip specified";
+    }
+  },
+  [
+  'description' => 'print info about ip'
+  ]
+);
+
+// joke
+$discord->registerCommand(
+  'joke',
+  function ($message){
+    $api_ret = file_get_contents("https://sv443.net/jokeapi/category/Programming");
+    if ( !isJson($api_ret) )
+      return "api error";
+    else {
+      $out  = "```";
+      $data = json_decode($api_ret,true);
+      if ($data["type"] == "twopart")
+      {
+        $out .= $data["setup"] . "\n";
+        $out .= $data["delivery"];
+      }
+      else
+      {
+        $out .= $data["joke"];
+      }
+      $out .= "```";
+      return $out;
+    }
+  },
+  [
+  'description' => 'a random programming joke'
+  ]
+);
+
+// iss
+$discord->registerCommand(
+  'iss',
+  function ($message){
+    $api_ret = file_get_contents("http://api.open-notify.org/iss-now.json");
+    if ( !isJson($api_ret) )
+      return "api error";
+    else {
+      $out  = "```";
+      $data = json_decode($api_ret,true);
+      $out .= "Longitude: " . $data["iss_position"]["longitude"] . "\n";
+      $out .= "Latitude:  " . $data["iss_position"]["latitude"];
+      $out .= "```";
+      return $out;
+    }
+  },
+  [
+  'description' => 'get current position of the International Space Station'
+  ]
+);
+
 // on bot ready
 $discord->on('ready', function ($discord) {
 
